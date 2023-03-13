f0a263f3c52c2a7d8ce55e7ad56d44c7f610ae76
Josh Hausotter <jhausotter@gmail.com>
2023-03-13 11:52:50 -0700

    fix part of menu, heros

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,33 +1,9 @@
-// This is where it all goes :)
-(function ($) {
-    'use strict';
+var body = document.querySelector('body')
+var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
+var menuContainer = document.querySelector('#main-menu-mobile');
 
-    $(document).ready(function () {
-        // DropDown menu on mobile devices
-        $('#mobile-menu__opener').on('click touchstart', function (e) {
-            e.preventDefault();
-
-            if (!$('#menu').hasClass('menu_slide_up')) {
-                $('#menu').addClass('menu_slide_up');
-            }
-
-            $('#menu').toggleClass('menu_slide_down');
-            $(this).toggleClass('mobile-menu__opener_menu_open');
-        });
-
-        // For resize window on desktop devices
-        $(window).resize(function () {
-            if ($(window).width() > 768) {
-                if ($('#menu').hasClass('menu_slide_up')) {
-                    $('#menu').removeClass('menu_slide_up');
-                }
-
-                if ($('#menu').hasClass('menu_slide_down')) {
-                    $('#menu').removeClass('menu_slide_down');
-                    $('#mobile-menu__opener').removeClass('mobile-menu__opener_menu_open');
-                }
-            }
-        });
-    });
-
-})(jQuery);
+menuTrigger.onclick = function() {
+    menuContainer.classList.toggle('open');
+    menuTrigger.classList.toggle('is-active')
+    body.classList.toggle('lock-scroll')
+}
